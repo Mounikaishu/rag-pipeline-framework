6aefb8fa076a997733b849265c43c3e83eb69645
Mounikaishu <24b01a1275@svecw.edu.in>
2026-06-09 12:10:08 +0530

    did required changes in ingestion

--- ingestion/chunker.py
+++ ingestion/chunker.py
@@ -1,21 +1,48 @@
-from langchain_text_splitters import (
-    RecursiveCharacterTextSplitter
+from langchain_experimental.text_splitter import (
+    SemanticChunker
 )
 
-from config.settings import Settings
+from langchain_huggingface import (
+    HuggingFaceEmbeddings
+)
+
+from langchain_core.documents import (
+    Document
+)
+
+from config.settings import (
+    Settings
+)
 
 
 class Chunker:
 
     def __init__(self):
 
-        self.text_splitter = (
-            RecursiveCharacterTextSplitter(
-                chunk_size=
-                Settings.CHUNK_SIZE,
+        self.headers = [
+            "Career Objective",
+            "Education",
+            "Projects",
+            "Skills",
+            "Achievements",
+            "Certifications",
+            "Experience"
+        ]
 
-                chunk_overlap=
-                Settings.CHUNK_OVERLAP
+        self.embedding_model = (
+            HuggingFaceEmbeddings(
+                model_name=
+                Settings.EMBEDDING_MODEL
+            )
+        )
+
+        self.semantic_splitter = (
+            SemanticChunker(
+                embeddings=
+                self.embedding_model,
+
+                breakpoint_threshold_type=
+                "percentile"
             )
         )
 
@@ -23,6 +50,71 @@ class Chunker:
         self,
         documents
     ):
-        return self.text_splitter.split_documents(
-            documents
-        )
+
+        all_chunks = []
+
+        for doc in documents:
+
+            text = doc.page_content
+
+            sections = (
+                self._header_chunk(
+                    text
+                )
+            )
+
+            for section in sections:
+
+                semantic_chunks = (
+                    self.semantic_splitter
+                    .create_documents(
+                        [section]
+                    )
+                )
+
+                all_chunks.extend(
+                    semantic_chunks
+                )
+
+        return all_chunks
+
+    def _header_chunk(
+        self,
+        text
+    ):
+
+        sections = []
+
+        current_section = ""
+
+        lines = text.split("\n")
+
+        for line in lines:
+
+            line = line.strip()
+
+            if line in self.headers:
+
+                if current_section:
+
+                    sections.append(
+                        current_section
+                    )
+
+                current_section = (
+                    line + "\n"
+                )
+
+            else:
+
+                current_section += (
+                    line + "\n"
+                )
+
+        if current_section:
+
+            sections.append(
+                current_section
+            )
+
+        return sections
--- main.py
+++ main.py
@@ -1,13 +1,191 @@
-from embeddings.embedding_model import EmbeddingModel
-
-
-embedding_model = EmbeddingModel()
-
-model = embedding_model.get_model()
-
-embedding = model.encode(
-    "What is SQL join?"
+from ingestion.pdf_loader import (
+    PDFLoader
 )
 
-print(len(embedding))
-print(embedding[:10])
+from ingestion.chunker import (
+    Chunker
+)
+
+from vectordb.chroma_store import (
+    ChromaStore
+)
+
+from retrieval.retriever import (
+    Retriever
+)
+
+from generation.generator import (
+    Generator
+)
+
+from rerank.reranker import (
+    Reranker
+)
+
+from refine.refiner import (
+    Refiner
+)
+
+from rewrite.query_rewriter import (
+    QueryRewriter
+)
+
+# --------------------------
+# STEP 1: LOAD PDF
+# --------------------------
+
+loader = PDFLoader()
+
+documents = loader.load_pdf(
+    "./cvx (2).pdf"
+)
+
+print(
+    "Loaded pages:",
+    len(documents)
+)
+
+
+# --------------------------
+# STEP 2: CHUNKING
+# --------------------------
+
+chunker = Chunker()
+
+chunks = chunker.split_documents(
+    documents
+)
+
+print(
+    "Chunks created:",
+    len(chunks)
+)
+
+
+# --------------------------
+# STEP 3: STORE
+# --------------------------
+
+vector_store = ChromaStore(
+    persist_directory=
+    "./test_db"
+)
+
+db = (
+    vector_store
+    .create_vector_store(
+        chunks
+    )
+)
+
+print(
+    "Embeddings stored!"
+)
+
+# --------------------------
+# STEP 4: QUERY REWRITE
+# --------------------------
+
+query = (
+    "What projects has she done?"
+)
+
+rewriter = (
+    QueryRewriter()
+)
+
+rewritten_query = (
+    rewriter.rewrite(
+        query
+    )
+)
+
+print(
+    "\nOriginal Query:",
+    query
+)
+
+print(
+    "Rewritten Query:",
+    rewritten_query
+)
+
+
+# --------------------------
+# STEP 5: RETRIEVE
+# --------------------------
+
+retriever = Retriever(
+    persist_directory=
+    "./test_db"
+)
+
+retrieved_docs = (
+    retriever.retrieve(
+        query=
+        rewritten_query,
+        k=10
+    )
+)
+
+print(
+    "\nRetrieved docs:",
+    len(retrieved_docs)
+)
+# --------------------------
+# STEP 5: RERANK
+# --------------------------
+
+reranker = Reranker()
+
+reranked_docs = (
+    reranker.rerank(
+        query=rewritten_query,
+        retrieved_docs=
+        retrieved_docs,
+        top_k=3
+    )
+)
+
+print(
+    "\nReranked docs:",
+    len(reranked_docs)
+)
+
+
+# --------------------------
+# STEP 6: REFINE
+# --------------------------
+
+refiner = Refiner()
+
+refined_docs = (
+    refiner.refine(
+        reranked_docs
+    )
+)
+
+print(
+    "\nRefined docs:",
+    len(refined_docs)
+)
+
+
+# --------------------------
+# STEP 7: GENERATE
+# --------------------------
+
+generator = Generator()
+
+answer = (
+    generator
+    .generate_answer(
+        query=rewritten_query,
+        retrieved_docs=
+        refined_docs
+    )
+)
+
+print("\nFinal Answer:\n")
+
+print(answer)

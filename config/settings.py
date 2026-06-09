f478a13ce98e39df1c2dec5f5fa8ece52c9d86b0
Mounikaishu <24b01a1275@svecw.edu.in>
2026-06-09 10:08:30 +0530

    Initial project setup

---
+++ config/settings.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Settings:
+
+    GROQ_API_KEY = os.getenv(
+        "GROQ_API_KEY"
+    )
+
+    LLM_MODEL = os.getenv(
+        "LLM_MODEL",
+        "llama-3.3-70b-versatile"
+    )
+
+    EMBEDDING_MODEL = os.getenv(
+        "EMBEDDING_MODEL",
+        "all-MiniLM-L6-v2"
+    )
+
+    CHUNK_SIZE = int(
+        os.getenv(
+            "CHUNK_SIZE",
+            500
+        )
+    )
+
+    CHUNK_OVERLAP = int(
+        os.getenv(
+            "CHUNK_OVERLAP",
+            100
+        )
+    )
